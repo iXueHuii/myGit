fbe9ae91110667678c29b745e8c660413e1816b7
xuehui <747576893@qq.com>
2020-07-14 11:51:45 +0800

    集成Generator

---
+++ src/main/java/com/wcjs/project/system/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.wcjs.project.system.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ * 用户信息表 前端控制器
+ * </p>
+ *
+ * @author xueHui
+ * @since 2020-07-14
+ */
+@Controller
+@RequestMapping("/system/user")
+public class UserController {
+
+}
---
+++ src/main/java/com/wcjs/project/system/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.wcjs.project.system.service;
+
+import com.wcjs.project.system.model.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户信息表 服务类
+ * </p>
+ *
+ * @author xueHui
+ * @since 2020-07-14
+ */
+public interface IUserService extends IService<User> {
+
+}
